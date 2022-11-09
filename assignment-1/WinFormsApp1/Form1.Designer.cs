8fceff9e5528735f1a20aac2b29b6c185f58cdbd
motasem-alhomsi <117853498+motasem-alhomsi@users.noreply.github.com>
2022-11-09 20:43:40 +0300

    Add files via upload

---
+++ assignment-1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(34, 110);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(244, 38);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(34, 204);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(244, 38);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "vize";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "final";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 124);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "hesap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "orta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(481, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 31);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "harf";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label5.Location = new System.Drawing.Point(343, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "geçtiniz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(481, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 31);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "kaldiniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 388);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ assignment-1/WinFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vize, final, ortalama;
+            vize = Convert.ToDouble(numericUpDown1.Value);
+            final = Convert.ToDouble(numericUpDown2.Value);
+            ortalama = vize * 0.4 + final * 0.6;
+            label3.Text = ortalama.ToString();
+
+            if (ortalama <= 39)
+            {
+                label4.Text = "FF";
+            }
+            else if (ortalama <= 49)
+            {
+                label4.Text = "FD";
+            }
+            else if (ortalama <= 54)
+            {
+                label4.Text = "DD";
+            }
+            else if (ortalama <= 59)
+            {
+                label4.Text = "DC";
+            }
+            else if (ortalama <= 69)
+            {
+                label4.Text = "CC";
+            }
+            else if (ortalama <= 74)
+            {
+                label4.Text = "CB";
+            }
+            else if (ortalama <= 79)
+            {
+                label4.Text = "BB";
+            }
+            else if (ortalama <= 84)
+            {
+                label4.Text = "BA";
+            }
+            else if (ortalama <= 100)
+            {
+                label4.Text = "AA";
+            }
+            if (ortalama >= 50 && final > 50)
+            {
+                label5.Visible = true;
+                label6.Visible = false;
+            }
+            else
+            {
+                label5.Visible = false;
+                label6.Visible = true;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            label6.Visible = false;
+        }
+    }
+}
